e8ddfbfcf825c8c6b56a9610a4fca06fe1b93fec
danyel duckman <danyel.duckman@gmail.com>
2023-03-28 17:02:50 +0200

    quete 04

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { FormsModule } from '@angular/forms';
 
@@ -7,10 +8,22 @@ import { AppComponent } from './app.component';
 import { BlockComponent } from './block/block.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { Quete03Component } from './quete03/quete03.component';
+import { ROUTES } from './app.routes';
+import { MenuComponent } from './menu/menu.component';
+import { SignupComponent } from './signup/signup.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
-  declarations: [AppComponent, BlockComponent, UserProfileComponent, Quete03Component],
-  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    BlockComponent,
+    UserProfileComponent,
+    Quete03Component,
+    MenuComponent,
+    SignupComponent,
+    UserComponent,
+  ],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(ROUTES)],
   providers: [],
   bootstrap: [AppComponent],
 })
