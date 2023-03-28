33d84856d36b0f4d1b57f25a61d07ebeacde1f5b
danyel duckman <danyel.duckman@gmail.com>
2023-03-28 14:00:03 +0200

    quete 02

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { BlockComponent } from './block/block.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @NgModule({
-  declarations: [AppComponent, BlockComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, BlockComponent, UserProfileComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
